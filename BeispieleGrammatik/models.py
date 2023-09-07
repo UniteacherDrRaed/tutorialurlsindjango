13cd1cf8d26bb5fda87357518982caacd06bc568
Dr. Raed <drraed.jb@gmail.com>
2023-09-07 19:13:43 +0200

    adding models

--- BeispieleGrammatik/models.py
+++ BeispieleGrammatik/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BeispielFuerArtikel(models.Model):
+    Artikel=models.CharField(max_length=10)
+    Typ=models.CharField(max_length=20)
+    Beispiel=models.CharField(max_length=200)
+      
+    def __str__(self):
+        return f"Artikel: {self.Artikel} Typ: {self.Typ} Beispiel: {self.Beispiel}"
+     
